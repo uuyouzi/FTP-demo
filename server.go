47124a57c2163be60a019754854486a4231cd1bd
uuyouzi <59443356+uuyouzi@users.noreply.github.com>
2026-06-16 22:37:00 +0800

    处理连接报错

--- server.go
+++ server.go
@@ -74,7 +74,7 @@ func (s *FTPserver) handerconn(conn net.Conn) {
 		line = strings.TrimSpace(line) //把字符串前后所有看不见的空白符号（空格、\r、\n、tab）统统擦掉
 		fmt.Println("收到命令:", line)
 
-		if strings.HasPrefix(line,"USER "){             //它是一个固定的前缀,客户端发过来的前缀,后面有个空格区分命令和参数 ，客户端发过来会是 USER youzi这样
+		if strings.HasPrefix(line,"USER "){             //USER它是一个固定的前缀,客户端发过来的前缀,后面有个空格区分命令和参数 ，客户端发过来会是 USER youzi这样
 
 			username = strings.TrimSpace(strings.TrimPrefix(line,"USER "))
 
@@ -82,7 +82,7 @@ func (s *FTPserver) handerconn(conn net.Conn) {
 			continue
 		}
 
-		if strings.HasPrefix(line,"PASS "){            //一个固定的前缀, 客户端发过来的前缀 这几行代码为了提取和验证账号密码用，但是有个坑windows客户端会先用匿名登录（发 USER anonymous），如果失败了才会弹窗让输入用户名密码。
+		if strings.HasPrefix(line,"PASS "){            //PASS  一个固定的前缀, 客户端发过来的前缀 这几行代码为了提取和验证账号密码用，但是有个坑windows客户端会先用匿名登录（发 USER anonymous），如果失败了才会弹窗让输入用户名密码。
 
 			password := strings.TrimSpace(strings.TrimPrefix(line,"PASS "))
 
@@ -99,29 +99,53 @@ func (s *FTPserver) handerconn(conn net.Conn) {
 		}
 
 		if !login {
-			conn.Write([]byte("530 请登录账号。 \r\n"))
+			conn.Write([]byte("530 请登录账号。 \r\n"))          //登录状态
 			continue
 		}
 
-		if line == "QUIT"{
-			conn.Write([]byte("221 Goodbye\r\n"))
+		if line == "PWD" {
+			conn.Write([]byte("257 \"/\" 是当前目录。\r\n"))              //257 对应FTP响应码 意思是创建路径名
+			continue
+		}
+
+		if line == "OPTS utf8 on"{                              //选项命令：启用 UTF‑8
+			conn.Write([]byte("200 成功\r\n"))
+			continue
+		}
+		if line == "SYST" {
+			conn.Write([]byte("215 UNIX Type: L8\r\n"))             //响应 "215 UNIX Type: L8"  意思是系统类型  可译为 “215 UNIX 类型：L8”，其中 L8 表示逻辑字节大小为 8 位（标准二进制流）
+			continue
+		}
+		if line == "SITE help" {
+			conn.Write([]byte("214 SITE help\r\n"))               //214对应帮助信息
+			continue
+		}
+		if line == "TYPE A" || line == "TYPE I" {              //客户端询问传输文本模式或者二进制模式
+			conn.Write([]byte("200 Type ok\r\n"))
+			continue
+		}
+		if line == "NOOP" {
+			conn.Write([]byte("200 NOOP ok.\r\n"))          //NOOP是心跳检测，检查服务器是否还活着这里返回一个200的命令
+			continue
+		}
+		if line == "PASV" {
+			conn.Write([]byte("227 进入被动模式 (127,0,0,1,4,20)\r\n"))     // 稍后实现真正的数据端口，现在先回个占位响应，防止客户端卡死
+			continue
+		}
+
+
+		if line == "QUIT" {
+			conn.Write([]byte("221 Goodbye\r\n"))           //结束连接  221表示 服务关闭控制连接
 			break
 		}else {
-			conn.Write([]byte("502 命令未实现。\r\n"))
+			conn.Write([]byte("502 命令未实现。\r\n"))          // 其他命令返回 502 未实现
 		}
 
 
 
 
 
-		//// 4. 处理命令（目前只是简单回显）
-		//if line == "QUIT" {
-		//	conn.Write([]byte("221 Goodbye\r\n"))
-		//	break
-		//} else {
-		//	// 其他命令返回 502 未实现
-		//	conn.Write([]byte("502 Command not implemented\r\n"))
-		//}
+
 	}
 
 }
